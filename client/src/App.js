c08008eb510a33db034091ef6ee72628c745efe6
Aiden Lee <aiden.lee.sd@gmail.com>
2025-03-17 23:56:19 -0700

    Fixed database issues, and everything else it all works now other than the front page which is cooked

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,6 @@
 import './App.css';
 import React from 'react';
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
-import DailyLog from "./pages/Daily_Log";
 import AddMemo from "./pages/AddMemo";
 import Update from "./pages/Update";
 import { ViewNotes } from './pages/ViewNotes';
@@ -22,7 +21,6 @@ function App() {
         </div>
 
         <Routes>
-          <Route path="/" element={<DailyLog />} />
           <Route path="/add" element={<AddMemo />} />
           <Route path="/update/:id" element={<Update />} />
           <Route path="/notes" element={<ViewNotes />} />
